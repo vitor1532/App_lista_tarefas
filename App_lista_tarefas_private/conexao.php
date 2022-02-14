be0f38bdd072c30e9bc085de224d68f29793058d
Vitor <vitomartinsalmeida@gmail.com>
2022-02-14 10:15:18 -0300

    criando classe conexao

---
+++ App_lista_tarefas_private/conexao.php
@@ -0,0 +1,66 @@
+<?php
+
+    class Conexao {
+
+        private $host = 'localhost';
+        private $dbname = 'php_com_pdo';
+        private $user = 'root';
+        private $pass = '';
+
+        public function conectar() {
+
+            try {
+
+                $conexao = new POD(
+                    "mysql:host-$this->host; dbname=$this->dbname",
+                    "$this->user",
+                    "$this->pass"
+                );
+
+                return $conexao;
+
+            } catch (PDOException $e) {
+                echo '<p>'.$e->getMessage().'</p>>';
+            }
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- App_lista_tarefas_private/tarefa.service.php
+++ App_lista_tarefas_private/tarefa.service.php
@@ -6,15 +6,19 @@
         public function inserir() {//create
 
         }
+
         public function recuperar() {//read
 
         }
+
         public function atualizar() {//update
 
         }
+
         public function remover() {//delete
 
         }
+
     }
 
 ?>

65df77cc5feb5ea73d34e95e55355f1478858402
unknown <vitomartinsalmeida@gmail.com>
2022-03-11 14:51:43 -0300

    atualzando status das tarefas

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -72,9 +72,9 @@
 
 			}
 
-			function atualizarStatus(id, status) {
+			function atualizarStatus(id) {
 
-				location.href='todas_tarefas.php?acao=atualizarStatus'
+				location.href = 'todas_tarefas.php?acao=atualizarStatus&id='+id;
 				
 			}
 
@@ -116,9 +116,15 @@
                                         	<?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)
                                         </div>
                                         <div class="col-sm-3 mt-2 d-flex justify-content-between">
-                                            <i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $tarefa->id ?>)"></i>
-                                            <i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
-                                            <i onclick="atualizarStatus(<?= $tarefa->id, $tarefa->status ?>)" class="fas fa-check-square fa-lg text-success attStatus"></i>
+                                            <i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $tarefa->id ?>)"></i>	
+
+                                            <? if($tarefa->status == 'pendente') {?>
+
+	                                            <i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
+	                                            <i class="fas fa-check-square fa-lg text-success attStatus" onclick="atualizarStatus(<?= $tarefa->id ?>)"></i>
+
+	                                        <? } ?>
+
                                         </div>
                                     </div>
 
